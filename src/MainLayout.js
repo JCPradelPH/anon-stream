98723913dd93bec6328918b0f591f464b150c98a
JC <pradeljc@gmail.com>
2018-03-02 14:38:37 +0800

    updated MainLayour file

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -2,9 +2,7 @@ import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import Routers from './Routers'
 export default class MainLayout extends React.Component{
-  componentDidMount(){
-
-  }
+  
   render(){
     return(
       <BrowserRouter>
